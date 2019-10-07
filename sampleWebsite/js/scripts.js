15107104559ceb5cd034ebf9ad02a13d6939b671
Pham Nguyen Quoc Anh <pnqanh@apcs.vn>
2019-10-07 17:37:30 +0700

    Add simple js script

---
+++ sampleWebsite/js/scripts.js
@@ -0,0 +1,7 @@
+function myFunction()
+{
+    console.log("clicked");
+    // console.log(document.getElementsByClassName("form__input")[0].value);
+    var x = document.getElementsByClassName("form__input")[0].value;
+    console.log(x);
+}
