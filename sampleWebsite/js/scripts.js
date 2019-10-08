a09f1e07ff0c0bbba84634b49bb0c603c2ada51d
Pham Nguyen Quoc Anh <pnqanh@apcs.vn>
2019-10-08 17:41:34 +0700

    Apply js to perform some actions, imrpove UI

--- sampleWebsite/js/scripts.js
+++ sampleWebsite/js/scripts.js
@@ -6,13 +6,31 @@ function myFunction() {
 }
 
 function testFunction1() {
-
+    var x = document.getElementById("navbar").getElementsByTagName("li");
+    console.log(x[0].innerText);
 }
 
-function testFunction2() {
+window.onscroll = function() {hide_btn_Top()};
 
+function hide_btn_Top() {
+    // console.log("clicked");
+    var btn_scroll = document.getElementsByClassName("up_icon")[0];
+    console.log(btn_scroll);
+// When the user scrolls down 20px from the top of the document, show the button
+    if (document.body.scrollTop > 360 || document.documentElement.scrollTop > 360) {
+        btn_scroll.style.display = "block";
+    } else {
+        btn_scroll.style.display = "none";
+    }
 }
 
-function testFunction3() {
+function scroll_Top() {
+    // When the user clicks on the button, scroll to the top of the document
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
+function linkTo() {
+    // var x = document.getElementById("gg");
+    window.open("http://wwww.goole.com/");
 }
