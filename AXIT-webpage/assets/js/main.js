b475185c8a837cf934b9deb219358994d017fe10
Pham Nguyen Quoc Anh <pnqanh@apcs.vn>
2019-10-16 14:18:20 +0700

    Done Body and Footer sections.

--- AXIT-webpage/assets/js/main.js
+++ AXIT-webpage/assets/js/main.js
@@ -1,3 +1,13 @@
-function openTab() {
-
+function openTab(evt, tab_id) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("text__content");
+    for (i = 0; i < text__content.length; i++) {
+        text__content[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("listitem__tablink");
+    for (i = 0; i < listitem__tablink.length; i++) {
+        listitem__tablink[i].className = listitem__tablink[i].className.replace(" active", "");
+    }
+    document.getElementById(tab_id).style.display = "block";
+    evt.currentTarget.className += " active";
 }
